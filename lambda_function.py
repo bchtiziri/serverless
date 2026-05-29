c2215583d09e4116baf0d7b739e4154622d07b7d
TiziriBch <bouchichenetiziri@gmail.com>
2026-05-29 13:32:53 +0200

    feat : ajout routes

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,31 @@
+import json
+from datetime import datetime
+
 def lambda_handler(event, context):
-    return {
-        "statusCode": 200,
-        "body": "Bonjour depuis AWS Lambda"
-    }
+    route = event.get("rawPath", "/")
+
+    if route == "/hello":
+        return {
+            "statusCode": 200,
+            "body": json.dumps({"message": "Bonjour depuis AWS Lambda !"})
+        }
+
+    elif route == "/time":
+        now = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
+        return {
+            "statusCode": 200,
+            "body": json.dumps({"heure_utc": now})
+        }
+
+    elif route == "/echo":
+        body = json.loads(event.get("body") or "{}")
+        return {
+            "statusCode": 200,
+            "body": json.dumps({"echo": body})
+        }
+
+    else:
+        return {
+            "statusCode": 404,
+            "body": json.dumps({"erreur": "Route introuvable"})
+        }
